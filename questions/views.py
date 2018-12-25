150a12862ddb605e3ba4aee62f95b64380d5f7da
Xenobyte42 <andmicha2011@bk.ru>
2018-12-25 03:44:23 +0300

    Forms now works

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage
@@ -15,8 +16,6 @@ popular_tags = ['Pigs', 'Evangelion', 'Cats',
 best_members = ['Kurt Cobain', 'Eyes Adrift', 'Blink-182',
                 'AC↯DC', '♪Metallica♪']
 
-user = Profile.objects.all()[0]
-
 def paginate(objects, request, amount):
     paginator = Paginator(objects, amount)
     page = request.GET.get('page')
@@ -30,7 +29,7 @@ class MainPage(View):
         question_list = Question.objects.get_list()
         visible_questions = paginate(question_list, request, settings.QUEST_PER_PAGE)
         context = {'questions': visible_questions, 'popular_tags': popular_tags,
-                   'best_members': best_members, 'user': user}
+                   'best_members': best_members, 'user': request.user}
         return render(request, self.template_name, context)
 
 
@@ -41,7 +40,7 @@ class HotPage(View):
         question_list = Question.objects.top()
         visible_questions = paginate(question_list, request, settings.QUEST_PER_PAGE)
         context = {'questions': visible_questions, 'popular_tags': popular_tags,
-                   'best_members': best_members, 'user': user}
+                   'best_members': best_members, 'user': request.user}
         return render(request, self.template_name, context)
 
 
@@ -52,7 +51,7 @@ class TagPage(View):
         question_list = Question.objects.get_tag(tag_name)
         visible_questions = paginate(question_list, request, settings.QUEST_PER_PAGE)
         context = {'questions': visible_questions, 'popular_tags': popular_tags,
-                   'best_members': best_members, 'user': user, 'tag_name': tag_name}
+                   'best_members': best_members, 'user': request.user, 'tag_name': tag_name}
         return render(request, self.template_name, context)
 
 
@@ -63,20 +62,10 @@ class QuestionPage(View):
         main_question = Question.objects.get_question(question_id)
         answers = Answer.objects.answer(question_id)
         context = {'main_question': main_question, 'answers': answers,
-                   'popular_tags': popular_tags, 'best_members': best_members, 'user': user}
+                   'popular_tags': popular_tags, 'best_members': best_members,
+                   'user': request.user}
         return render(request, self.template_name, context)
 
-
-class LoginPage(View):
-    template_name = 'registration/login.html'
-    form_class = LoginForm
-
-    def get(self, request):
-        form = self.form_class()
-        context = {'popular_tags': popular_tags,
-                   'best_members': best_members,
-                   'form': form}
-        return render(request, self.template_name, context)
 
 class SignupPage(View):
     template_name = 'signup.html'
@@ -89,11 +78,25 @@ class SignupPage(View):
                    'form': form}
         return render(request, self.template_name, context)
 
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.set_password(user.password)
+            user.save()
+            return redirect('login')
+        context = {'popular_tags': popular_tags,
+                   'best_members': best_members,
+                   'form': form}
+        return render(request, self.template_name, context)
+
 
 class AskPage(View):
     template_name = 'ask.html'
     form_class = AskForm
 
+    @method_decorator(login_required)
     def get(self, request):
         form = self.form_class()
         context = {'popular_tags': popular_tags,
@@ -106,8 +109,20 @@ class SettingsPage(View):
     template_name = 'settings.html'
     form_class = SettingsForm
 
+    @method_decorator(login_required)
     def get(self, request):
-        form = self.form_class()
+        form = self.form_class(instance=request.user)
+        context = {'popular_tags': popular_tags,
+                   'best_members': best_members,
+                   'form': form}
+        return render(request, self.template_name, context)
+
+    @method_decorator(login_required)
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES, instance=request.user)
+        if form.is_valid:
+            user = form.save(commit=False)
+            user.save()
         context = {'popular_tags': popular_tags,
                    'best_members': best_members,
                    'form': form}
